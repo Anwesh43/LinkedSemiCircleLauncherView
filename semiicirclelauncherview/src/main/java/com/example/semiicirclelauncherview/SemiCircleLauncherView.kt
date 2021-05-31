51924d60cde3479a9a36df1090aa9250c7ef252c
Anwesh43 <anweshthecool0@gmail.com>
2021-05-31 15:32:07 +0530

    adding Canvas extension methods

--- semiicirclelauncherview/src/main/java/com/example/semiicirclelauncherview/SemiCircleLauncherView.kt
+++ semiicirclelauncherview/src/main/java/com/example/semiicirclelauncherview/SemiCircleLauncherView.kt
@@ -31,3 +31,41 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSemiCircleLaunch(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        rotate(180f * sf3)
+        translate(-size, 0f)
+        drawArc(
+            RectF(-size, -size / 2, size, size / 2),
+            0f,
+            180f * sf1,
+            false,
+            paint
+        )
+        drawCircle(-size / 2, size / 4 - (h / 2 + size) * sf4, r * sf2, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSCLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / sizeFactor
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSemiCircleLaunch(scale, w, h, paint)
+}
