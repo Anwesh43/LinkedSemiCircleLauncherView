9c0e7e68d36fd1829254c4ff2ae0c6d9e63cc46d
Anwesh43 <anweshthecool0@gmail.com>
2021-05-31 17:02:58 +0530

    adding SemiCircleLauncherView class

--- semiicirclelauncherview/src/main/java/com/example/semiicirclelauncherview/SemiCircleLauncherView.kt
+++ semiicirclelauncherview/src/main/java/com/example/semiicirclelauncherview/SemiCircleLauncherView.kt
@@ -69,3 +69,19 @@ fun Canvas.drawSCLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSemiCircleLaunch(scale, w, h, paint)
 }
+
+class SemiCircleLauncherView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
