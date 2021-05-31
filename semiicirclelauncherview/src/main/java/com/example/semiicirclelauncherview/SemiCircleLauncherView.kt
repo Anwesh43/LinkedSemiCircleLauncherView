aecdfd04549a95e707bc800043f7cd816a0f62da
Anwesh43 <anweshthecool0@gmail.com>
2021-05-31 17:31:56 +0530

    addng SemiCircleLaunch class

--- semiicirclelauncherview/src/main/java/com/example/semiicirclelauncherview/SemiCircleLauncherView.kt
+++ semiicirclelauncherview/src/main/java/com/example/semiicirclelauncherview/SemiCircleLauncherView.kt
@@ -173,4 +173,27 @@ class SemiCircleLauncherView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SemiCircleLaunch(var i : Int) {
+
+        private var curr : SCLNode = SCLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
