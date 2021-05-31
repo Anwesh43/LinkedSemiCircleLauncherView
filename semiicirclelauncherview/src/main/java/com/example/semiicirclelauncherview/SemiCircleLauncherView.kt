76f576f4164e2c547da9638ab57affac8134756b
Anwesh43 <anweshthecool0@gmail.com>
2021-05-31 11:49:41 +0530

    adidng Int and Float extension methods

--- semiicirclelauncherview/src/main/java/com/example/semiicirclelauncherview/SemiCircleLauncherView.kt
+++ semiicirclelauncherview/src/main/java/com/example/semiicirclelauncherview/SemiCircleLauncherView.kt
@@ -27,3 +27,7 @@ val parts : Int = 4
 val scGap : Float = 0.02f / parts
 val deg : Float = 180f
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
